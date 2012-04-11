14a59e76a793307caf62374cdee034b056554136
Unai Molina <unai.molina.gordillo@gmail.com>
2012-04-11 21:36:37 +0100

    removed demo bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,12 +17,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new Acme\HelloBundle\AcmeHelloBundle(),
             new Store\AlbumBundle\StoreAlbumBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
